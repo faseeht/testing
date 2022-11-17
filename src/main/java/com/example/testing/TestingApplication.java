544dd7f3baaa2a07fb51ed5c28866b2080149c5e
Faseeh Tahir <faseeh.tahir@arittek.com>
2022-11-17 14:33:09 +0500

    Default testing api

--- src/main/java/com/example/testing/TestingApplication.java
+++ src/main/java/com/example/testing/TestingApplication.java
@@ -2,10 +2,17 @@ package com.example.testing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestingApplication {
 
+    @GetMapping("/")
+    public String getStatus(){
+        return "Application Running.";
+    }
     public static void main(String[] args) {
         SpringApplication.run(TestingApplication.class, args);
     }
